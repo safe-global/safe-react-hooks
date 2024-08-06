c41197af2d96f5009afc854f0e4cc6efcb60def0
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2024-08-06 12:19:56 +0200

    Fix tests for `useSafeInfo` hook

--- src/hooks/useSafeInfo.test.ts
+++ src/hooks/useSafeInfo.test.ts
@@ -3,7 +3,7 @@ import { SafeClient } from '@safe-global/safe-kit'
 import { useSafeInfo } from '@/hooks/useSafeInfo.js'
 import * as useSafeClient from '@/hooks/useSafeClient.js'
 import { configExistingSafe, configPredictedSafe } from '@test/config.js'
-import { safeInfo } from '@test/fixtures.js'
+import { safeInfo, signerPrivateKeys } from '@test/fixtures.js'
 import { renderHookInSafeProvider } from '@test/utils.js'
 
 describe('useSafeInfo', () => {
@@ -24,46 +24,54 @@ describe('useSafeInfo', () => {
 
   afterEach(() => {
     jest.clearAllMocks()
-    jest.resetAllMocks()
   })
 
   it('should return fetch and return Safe infos via SafeClient', async () => {
-    const { result } = renderHookInSafeProvider(() => useSafeInfo(), { config: configExistingSafe })
+    const { result } = renderHookInSafeProvider(() => useSafeInfo(), {
+      config: configExistingSafe
+    })
 
     expect(useSafeClientSpy).toHaveBeenCalledTimes(1)
-    expect(useSafeClientSpy).toHaveBeenCalledWith()
+    expect(useSafeClientSpy).toHaveBeenCalledWith(undefined)
 
     expect(result.current).toMatchObject({ data: undefined, status: 'pending' })
 
     await waitFor(() => expect(result.current).toMatchObject({ data: safeInfo, status: 'success' }))
-
-    expect(useSafeClientSpy).toHaveBeenCalledTimes(2)
-    expect(useSafeClientSpy).toHaveBeenCalledWith()
 
     expect(safeClientMock.protocolKit.getAddress).toHaveBeenCalledTimes(1)
     expect(safeClientMock.protocolKit.getNonce).toHaveBeenCalledTimes(1)
     expect(safeClientMock.protocolKit.getThreshold).toHaveBeenCalledTimes(1)
     expect(safeClientMock.protocolKit.isSafeDeployed).toHaveBeenCalledTimes(1)
     expect(safeClientMock.protocolKit.getOwners).toHaveBeenCalledTimes(1)
+
+    expect(useSafeClientSpy).toHaveBeenCalledTimes(2)
+    expect(useSafeClientSpy).toHaveBeenCalledWith(undefined)
   })
 
   it('should accept a config to override the one from the SafeProvider', async () => {
-    renderHookInSafeProvider(() => useSafeInfo({ config: configPredictedSafe }), {
-      config: configExistingSafe
-    })
+    const { result } = renderHookInSafeProvider(
+      () => useSafeInfo({ config: configPredictedSafe }),
+      { config: configExistingSafe }
+    )
 
     expect(useSafeClientSpy).toHaveBeenCalledTimes(1)
     expect(useSafeClientSpy).toHaveBeenCalledWith({ config: configPredictedSafe })
+
+    await waitFor(() => expect(result.current).toMatchObject({ data: safeInfo, status: 'success' }))
+
+    expect(useSafeClientSpy).toHaveBeenCalledTimes(2)
+    expect(useSafeClientSpy).toHaveBeenCalledWith({ config: configPredictedSafe })
   })
 
-  // TODO: fix this failing test
-  it.skip('should return no data if any request fails', async () => {
+  it('should return no data if any request fails', async () => {
     safeClientMock.protocolKit.getAddress.mockRejectedValue(new Error('Address error'))
 
-    const { result } = renderHookInSafeProvider(() => useSafeInfo(), { config: configExistingSafe })
+    const { result } = renderHookInSafeProvider(() => useSafeInfo(), {
+      config: { ...configPredictedSafe, signer: signerPrivateKeys[1] }
+    })
 
     expect(useSafeClientSpy).toHaveBeenCalledTimes(1)
-    expect(useSafeClientSpy).toHaveBeenCalledWith()
+    expect(useSafeClientSpy).toHaveBeenCalledWith(undefined)
 
     expect(result.current).toMatchObject({ data: undefined, status: 'pending' })
   })
