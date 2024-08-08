e105b4e055a77cf00fbd64398d9ceffb09742a51
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2024-08-08 20:53:05 +0200

    Update `useSignerAddress` hook to use the signerClient

--- src/hooks/useSafe.ts
+++ src/hooks/useSafe.ts
@@ -3,22 +3,22 @@ import type { ConfigParam, SafeConfig } from '@/types/index.js'
 import { useBalance } from '@/hooks/useBalance.js'
 import { useChain } from '@/hooks/useChain.js'
 import { useSafeInfo } from '@/hooks/useSafeInfo.js'
+import { useSignerAddress } from '@/hooks/useSignerAddress.js'
 import { SafeContext } from '@/SafeProvider.js'
 
 export type UseSafeParams = ConfigParam<SafeConfig>
 
 /**
  * Top-level hook to get Safe-related information.
- * @param params Parameters to customize the hook behavior.
- * @param params.config SafeConfig to use instead of the one provided by `SafeProvider`.
  * @returns Object wrapping the Safe hooks.
  */
+export function useSafe() {
   const { initialized } = useContext(SafeContext)
   return {
     initialized,
-    getBalance: () => useBalance({ config }),
-    getChain: () => useChain({ config }),
-    getSafeInfo: () => useSafeInfo({ config }),
-    getSignerAddress: () => useSignerAddress({ config })
+    getBalance: useBalance,
+    getChain: useChain,
+    getSafeInfo: useSafeInfo,
+    getSignerAddress: useSignerAddress
   }
 }
--- src/hooks/useSignerAddress.ts
+++ src/hooks/useSignerAddress.ts
@@ -1,27 +1,31 @@
 import { useEffect, useState } from 'react'
 import type { Address } from 'viem'
+import { ConfigParam, SafeConfig, RequiredProp } from '@/types/index.js'
 import { useConfig } from '@/hooks/useConfig.js'
-import { useSafeClient } from '@/hooks/useSafeClient.js'
-import type { ConfigParam, SafeConfig } from '@/types/index.js'
+import { useSignerClient } from '@/hooks/useSignerClient.js'
 
-export type UseSignerAddressParams<Config extends SafeConfig = SafeConfig> = ConfigParam<Config>
+export type UseSignerAddressParams = ConfigParam<RequiredProp<SafeConfig, 'signer'>>
 export type UseSignerAddressReturnType = Address | undefined
 
 /**
  * Hook to get the configured signer's address.
  * @param params Parameters to customize the hook behavior.
  * @param params.config SafeConfig to use instead of the one provided by `SafeProvider`.
- * @returns Signer address or undefined if not available.
+ * @returns Signer address or `undefined` if not available.
  */
+export function useSignerAddress(params: UseSignerAddressParams = {}): UseSignerAddressReturnType {
   const [config] = useConfig({ config: params.config })
 
+  const signerClient = useSignerClient({ config })
+  const [signerAddress, setSignerAddress] = useState<UseSignerAddressReturnType>()
+
   useEffect(() => {
-    safeClient?.protocolKit.getSafeProvider().getSignerAddress().then(setSigner)
-  }, [safeClient])
+    if (signerClient) {
+      signerClient?.protocolKit.getSafeProvider().getSignerAddress().then(setSignerAddress)
+    } else {
+      setSignerAddress(undefined)
+    }
+  }, [signerClient])
 
-  if (!config.signer) {
-    throw new Error('Signer not configured')
-  }
-
-  return signer
+  return signerAddress
 }
