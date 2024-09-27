ae4af8598219ef168ade5a4f755f5eea8ef57f1c
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2024-09-27 12:00:35 +0200

    Add `useUpdateOwners` hook for managing owners of the connected Safe
    It wraps the individual hooks:
    - `useAddOwner`
    - `useRemoveOwner`
    - `useSwapOwner`

---
+++ src/hooks/useUpdateOwners/index.ts
@@ -0,0 +1,24 @@
+import { ConfigParam, SafeConfigWithSigner } from '@/types/index.js'
+import { useAddOwner } from './useAddOwner.js'
+import { useRemoveOwner } from './useRemoveOwner.js'
+import { useSwapOwner } from './useSwapOwner.js'
+
+export type UseAddOwnerParams = ConfigParam<SafeConfigWithSigner>
+
+export type UseUpdateOwnersReturnType = {
+  add: ReturnType<typeof useAddOwner>
+  remove: ReturnType<typeof useRemoveOwner>
+  swap: ReturnType<typeof useSwapOwner>
+}
+
+/**
+ * Hook to add, remove or swap owners of the connected Safe.
+ * @returns Object wrapping the hooks to update, remove or swap owners.
+ */
+export function useUpdateOwners(params: UseAddOwnerParams = {}): UseUpdateOwnersReturnType {
+  const add = useAddOwner({ config: params.config })
+  const remove = useRemoveOwner({ config: params.config })
+  const swap = useSwapOwner({ config: params.config })
+
+  return { add, remove, swap }
+}
