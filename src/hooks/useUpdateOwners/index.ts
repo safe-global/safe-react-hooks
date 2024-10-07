0395fb294d2c151c02ec73560e91a4436237549b
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2024-10-07 14:58:38 +0200

    Fix name of `useUpdateOwners` hook's param type

--- src/hooks/useUpdateOwners/index.ts
+++ src/hooks/useUpdateOwners/index.ts
@@ -3,7 +3,7 @@ import { useAddOwner } from './useAddOwner.js'
 import { useRemoveOwner } from './useRemoveOwner.js'
 import { useSwapOwner } from './useSwapOwner.js'
 
-export type UseAddOwnerParams = ConfigParam<SafeConfigWithSigner>
+export type UseUpdateOwnersParams = ConfigParam<SafeConfigWithSigner>
 
 export type UseUpdateOwnersReturnType = {
   add: ReturnType<typeof useAddOwner>
@@ -15,7 +15,7 @@ export type UseUpdateOwnersReturnType = {
  * Hook to add, remove or swap owners of the connected Safe.
  * @returns Object wrapping the hooks to update, remove or swap owners.
  */
-export function useUpdateOwners(params: UseAddOwnerParams = {}): UseUpdateOwnersReturnType {
+export function useUpdateOwners(params: UseUpdateOwnersParams = {}): UseUpdateOwnersReturnType {
   const add = useAddOwner({ config: params.config })
   const remove = useRemoveOwner({ config: params.config })
   const swap = useSwapOwner({ config: params.config })
