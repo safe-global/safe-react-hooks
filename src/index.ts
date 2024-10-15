6dd593e6a2a1ec956bb85d8144271d67d1080cb4
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2024-10-15 11:34:13 +0200

    chore: Add hook types to exports
    Adding paramter and return types of hooks to exports.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-export {
-  useConfirmTransaction,
-  useSafe,
-  useSendTransaction,
-  useUpdateOwners,
-  useUpdateThreshold
-} from '@/hooks/index.js'
+export * from '@/hooks/useSafe.js'
+export * from '@/hooks/useConfirmTransaction.js'
+export * from '@/hooks/useSendTransaction.js'
+export * from '@/hooks/useUpdateOwners/index.js'
+export * from '@/hooks/useUpdateThreshold.js'
+
 export * from '@/types/index.js'
+
 export { SafeProvider } from './SafeProvider.js'
 export { SafeContext } from './SafeContext.js'
 export { createConfig } from './createConfig.js'
